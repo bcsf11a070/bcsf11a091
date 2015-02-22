79b6fafc6736550b64604289bf9cf8deaa48048d
bcsf11a070 <bcsf11a070@pucit.edu.pk>
2015-02-22 23:22:08 +0500

    first

---
+++ Scripts/custom.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+
+    $("#pp").click(function () {
+        var userName = $('#un').val();
+
+        $.getJSON("/Admin/CheckUserName?UserName=" + userName, function (data) {
+
+            if (data) {
+                $('#status').text("Valid username!");
+
+            }
+            else {
+                $('#status').text("Invalid username!");
+            }
+        });
+    });
+
+
+
+});
